513d415bcec46dc15ae31e2a2125f2f60c8338ad
manuelpezzi <pezzimanuel@gmail.com>
2025-05-13 11:23:11 +0200

    bonus uno completato e testato

--- index.php
+++ index.php
@@ -34,7 +34,12 @@ class Movie {
     // Metodo per ottenere i dettagli dei film
 
     public function getMovieDetails(){
-        return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}, Genere: {$this->genre->name}";
+        $genreNames = array_map(function($genre){
+            return $genre->name;
+        },$this->genres);
+        $genresString = implode(",",$genreNames);
+        return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}, Generi: {$genresString}";
+     
     }
 
 }
@@ -43,19 +48,28 @@ class Movie {
 
 $actionGenre = new Genre("Azione", "Film con scene di combattimento e adrenalina");
 $dramaGenre = new Genre ("Drammatico", "Film che esplorano emozioni profonde");
+$adventureGenre = new Genre("Avventura", "Film con esplorazioni e viaggi emozionanti");
+
 
 //creazione di istanzw della classe Movie
 
-$movie1 = new Movie ("Mad Max: Fury Road", "George Miller", 2015, $actionGenre);
-$movie2 = new Movie ("The Shawshank Redemption", "Frank Darabont", 1994, $dramaGenre);
+$movie1 = new Movie ("Mad Max: Fury Road", "George Miller", 2015, [$actionGenre,$adventureGenre]);
+$movie2 = new Movie ("The Shawshank Redemption", "Frank Darabont", 1994, [$dramaGenre]);
 
-//stampa dei valori delle proprietà dei film
-
+// Stampa dei valori delle proprietà dei film
 echo "<h2>Dettagli Film 1</h2>";
 echo "<p>" . $movie1->getMovieDetails() . "</p>";
-echo "<p>Descrizione genere: " . $movie1->genre->description . "</p>";
+echo "<p>Descrizioni generi: ";
+$genreDescriptions = array_map(function($genre) {
+    return $genre->description;
+}, $movie1->genres);
+echo implode("; ", $genreDescriptions) . "</p>";
 
 echo "<h2>Dettagli Film 2</h2>";
 echo "<p>" . $movie2->getMovieDetails() . "</p>";
-echo "<p>Descrizione genere: " . $movie2->genre->description . "</p>";
+echo "<p>Descrizioni generi: ";
+$genreDescriptions = array_map(function($genre) {
+    return $genre->description;
+}, $movie2->genres);
+echo implode("; ", $genreDescriptions) . "</p>";
 ?>
